025cbd35a2add4f3cba8c1b2cd91835124f96d65
mazmulj <mjmohammed@mun.ca>
2022-07-29 03:58:15 -0230

    Update main.js

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// VARIABLES TO SHOW MENU AND HIDE MENU
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+// SHOW MENU
+if (navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('showMenu')
+    }) 
+}
+
+// HIDE MENU
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('showMenu')
+    })
+}
+
+// HIDE MENU AFTER CLICKING NAV ITEM
+const navLink = document.querySelectorAll('.nav__link');
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    nav-menubar.classList.remove('showMenu');
+}
+navLink.addEventListener('click', linkAction);
