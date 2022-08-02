4e7586f53e7c4e0d0b5b88ca9b618c821f51556a
mazmulj <mjmohammed@mun.ca>
2022-08-02 16:59:53 -0230

    z

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,18 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active')
     })
 })
+
+// PORTFOLIO SWIPER
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
