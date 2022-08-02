b30ce6c67e99b83cc27afd41061afba31b6113ff
mazmulj <mjmohammed@mun.ca>
2022-08-02 01:33:43 -0230

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,23 @@ function toggleSkills(){
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
+
+// QUALIFICATION TABS
+const tabs = document.querySelectorAll('[data-target]');
+const tabContent = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContent.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
