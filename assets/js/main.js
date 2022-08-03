c700f18714add43d821fbf0c5c3aa49f28a8e093
mazmulj <mjmohammed@mun.ca>
2022-08-02 22:27:39 -0230

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,7 @@ tabs.forEach(tab => {
 let swiper = new Swiper(".portfolio__container", {
     cssMode: true,
     loop: true,
-    
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -78,3 +78,24 @@ let swiper = new Swiper(".portfolio__container", {
       clickable: true,
     },
   });
+
+// SCROLL ACTIVE NAV BAR
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop+sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active-link')
+        }
+        else{
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
